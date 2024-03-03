adf478a3ff000dbd33b1fc7fa5036d8637ac6aed
Lennart <mailtolennart@gmx.de>
2024-03-03 22:54:14 +0100

    add dry run only mode

--- backup.sh
+++ backup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Check if both source and destination directories are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <source_directory> <destination_directory>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <source_directory> <destination_directory> [--dry-run]"
     exit 1
 fi
 
 SOURCE_DIR="$1"
 DESTINATION_DIR="$2"
+DRY_RUN=false
+
+# Check if the optional parameter --dry-run is provided
+if [ "$#" -eq 3 ] && [ "$3" == "--dry-run" ]; then
+    DRY_RUN=true
+fi
 
 # Check if source and destination directories exist
 if [ ! -d "$SOURCE_DIR" ]; then
@@ -22,15 +28,17 @@ fi
 
 echo "Start backup: '$SOURCE_DIR' to '$DESTINATION_DIR'"
 
-# Set options for rsync dry run
-DRY_RUN_OPTIONS="-avn --progress --no-perms --delete"
-
-# Set options for rsync actual run
+# Set options for rsync run
 RUN_OPTIONS="-av --progress --no-perms --delete"
 
 # Perform dry run
 echo "Performing dry run..."
-rsync $DRY_RUN_OPTIONS "$SOURCE_DIR" "$DESTINATION_DIR"
+rsync $RUN_OPTIONS --dry-run "$SOURCE_DIR" "$DESTINATION_DIR"
+
+# Perform dry run if specified
+if [ "$DRY_RUN" = true ]; then
+    exit
+fi
 
 # Prompt for confirmation
 read -p "Proceed backup? (y/n): " choice
